bd2e96046ea960e7b7744c195ce86d66ba41b1ca
EmmaBergner <bergner.emma@gmail.com>
2022-05-09 14:10:01 +0000

    Add controlpage with url

--- state/admin.py
+++ state/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 from .models import State
-from django_summernote.admin import SummernoteModelAdmin
 
-@admin.register(State)
-class StateAdmin(SummernoteModelAdmin):
-
-    summernote_fields = ('content')
+admin.site.register(State)
 
--- state/views.py
+++ state/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from.views import generic
+from .models import State 
+from django.views.generic.detail import DetailView
 
 # Create your views here.
+class ControlPage(DetailView):
+    model = State 
+    template_name = 'business.html'
+

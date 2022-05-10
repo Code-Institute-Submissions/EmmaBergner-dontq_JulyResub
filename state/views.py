af073f4219b9f343698082bf36720ebaa6001aaf
EmmaBergner <bergner.emma@gmail.com>
2022-05-10 19:09:38 +0000

    Add html under templates

--- state/views.py
+++ state/views.py
@@ -6,10 +6,11 @@ from .models import State
 class ControlPage(View):
     model = State 
     template_name = 'business.html'
+    template_name = 'user.html'
 
     def get(self, request, *args, **kwargs):
         queryset = State.objects.all()
         state = get_object_or_404(queryset, id=2)
         
         return render(request, 'business.html')
-
+        return render(request, 'user.html')

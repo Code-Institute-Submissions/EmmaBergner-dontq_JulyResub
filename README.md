740124d78c2cbb6332b143b8bd2892d59a506d0d
EmmaBergner <bergner.emma@gmail.com>
2022-05-16 16:06:52 +0000

    Implement previous-button feature on webpage for business

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # dontq
 
 
+[DontQ](https://emmabergner.github.io/blink/) is a web browser application for digital queing tickets. Insted of printing out tickets the customer scan a UR-code and it's queue number is displayed.  
+
 
 Credits
 
--- state/views.py
+++ state/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from .models import State 
+from .models import State
 
 # Create your views here.
+
+
 class ControlPage(View):
-    model = State 
+    model = State
     template_name = 'business.html'
     template_name = 'user.html'
 
     def get(self, request, *args, **kwargs):
+        print("hello")
+        
         queryset = State.objects.all()
         state = get_object_or_404(queryset, id=2)
+        if (request.GET.get('mybtn')):
+            print("world")
+            state.current = state.current+1
+            state.save()
         context = {
             'current': state.current
         }
         return render(request, 'business.html', context)
-        # return render(request, 'user.html')
+
+
+
+def request_page(request):
+    if(request.GET.get('mybtn')):
+        mypythoncode.mypythonfunction(int(request.GET.get('mytextbox')))
+    return render(request, 'myApp/templateHTML.html')

39efd87033bb104e8897a23be22d3798b25343ae
EmmaBergner <bergner.emma@gmail.com>
2022-05-27 12:53:23 +0000

    Add register page

--- state/urls.py
+++ state/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('user', views.UserPage.as_view(), name="User"),
     path('usertwo', views.UserTwoPage.as_view(), name="User"),
     path('accounts/login/', views.Login.as_view(), name="Login"),
+    path('register', views.Register.as_view(), name="Register"),
 ]
--- state/views.py
+++ state/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.utils.decorators import method_decorator
 from django.contrib.auth import logout
+from django.contrib import messages
 
 
 # Create your views here.
@@ -16,9 +17,7 @@ class ControlPage(View):
     template_name = 'user.html'
 
     def get(self, request, *args, **kwargs):
-        print(request.user)
-        queryset = State.objects.all()
-        state = get_object_or_404(queryset, id=2)
+        state = get_object_or_404(State, business = request.user)
         if (request.GET.get('mybtn') and state.current > 0):
             state.current = state.current-1
         if (request.GET.get('mybtng')):
@@ -48,32 +47,57 @@ class Login(View):
             login(request, user)
             return redirect('/')
         else:
+            messages.error(request,"Login Failed. Please try again.")
             # Return an 'invalid login' error message.
             print("Fel password " + request.POST.get('password'))
             print("Fel username " + request.POST.get('username'))
             return render(request, 'login.html')
        
 
+class Register(View):  
+    def get(self, request, *args, **kwargs):
+        return render(request, 'register.html')
+
+    def post(self, request, *args, **kwargs):
+        businessName = request.POST['businessName']
+        owner = request.POST['owner']
+        passwordOne = request.POST['passwordOne']
+        passwordTwo = request.POST['passwordTwo']
+        user = authenticate(request, username=username, password=password)
+        
+
+
+
+
 class UserPage(View):
     model = State
     template_name = 'business.html'
     template_name = 'user.html'
 
+    # URL, for example: .../user?baronen
     def get(self, request, *args, **kwargs):
-        queryset = State.objects.all()
-        state = get_object_or_404(queryset, id=2)
-        state.next = state.next+1
+        businessName = request.get_full_path().split("?")[1]
+        state = get_object_or_404(State, business__username = businessName)
+        ticket = state.next
+        if state.current > ticket:
+            ticket = state.current 
+        state.next = ticket +1
         state.save()
-        return redirect('/usertwo?' + str(state.next))
+        return redirect('/usertwo?' + str(ticket) + "&" + businessName) # URL, for example: .../usertwo?46&Baronen
 
 
 class UserTwoPage(View):
     model = State
 
+    # URL, for example: .../usertwo?46&Baronen
     def get(self, request, *args, **kwargs):
-        queryset = State.objects.all()
-        state = get_object_or_404(queryset, id=2)
-        ticket = request.get_full_path().split("?")[1]
+        url = request.get_full_path() # ".../usertwo?46&Baronen"
+        parts = url.split("?") # [".../usertwo", "46&Baronen"]
+        queryString = parts[1] # 46&Baronen
+        partstwo = queryString.split("&")      # ["46", "Baronen"]
+        ticket = partstwo[0]      # 46
+        businessName = partstwo[1]     # Baronen
+        state = get_object_or_404(State, business__username = businessName)
         context = {
             'current': state.current,
             'ticket': ticket,

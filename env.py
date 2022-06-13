97338d49f35b5aa1b3fe798460edb52bfa7aeda4
EmmaBergner <bergner.emma@gmail.com>
2022-06-13 14:23:54 +0000

    Make background image work on Heroku

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://ylzkoepnwedpko:fe93ca9e704be20deebabf119ceaf2eca478e9368ae5ceaf551785eca479ffad@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/d6mc7mfngt9bpl"
 os.environ["SECRET_KEY"] = "dontQueue2022!"
+os.environ["CLOUDINARY_URL"] = "cloudinary://379862189571849:TtFCZaGaTaH9q1-OlTtVRYgVlAY@dontq"
